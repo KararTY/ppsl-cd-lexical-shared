343dbc67d7c35e5221712ac4ad897caf77825856
KararTY <alremahykarar@ymail.com>
2023-05-14 23:05:37 +0200

    fix: Refactor BioEditor for reusability

--- src/editors/Bio/editor.jsx
+++ src/editors/Bio/editor.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { encode } from '@msgpack/msgpack'
 
 import { LexicalComposer } from '@lexical/react/LexicalComposer'
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
@@ -11,49 +10,24 @@ import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin'
 import { TreeViewPlugin } from '../plugins/TreeView/plugin'
 
 import { defaultTheme, editableEditorTheme, readOnlyTheme } from '../theme'
+import { bioConfig } from './config'
 import { Toolbar } from '../toolbar/index'
 import { Placeholder } from '../components/placeholder'
 import { Editor } from '../components/editor'
 import { EditorFooter } from '../components/footer'
-import { bioConfig } from './config'
 
 /**
  * @param {{readOnly, post, initialContent}} props
  */
 export function BioEditor (props) {
-  const { readOnly = false, post = {}, initialContent } = props
+  const { readOnly = false, title, post = {}, initialContent, onSubmit } = props
 
   const [isSaving, setIsSaving] = useState(false)
 
-  const onSubmitBio = async ({ event, editor }) => {
-    event.preventDefault()
-
-    const content = editor.getEditorState().toJSON()
-    const encodedContent = encode(content).toString()
-
-    const headers = new Headers()
-    headers.append('content-type', 'application/json')
-
+  const onSubmitCatch = async ({ event, editor }) => {
     setIsSaving(true)
-
-    try {
-      const res = await fetch('/api/users/', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          content: encodedContent
-        })
-      })
-
-      console.log(await res.text())
-
-      if (res.status >= 200 && res.status < 300) window.location.reload()
-      else throw res
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setIsSaving(false)
-    }
+    await onSubmit({ event, editor })
+    setIsSaving(false)
   }
 
   const config = bioConfig(defaultTheme, !readOnly, function onError (error) {
@@ -70,11 +44,13 @@ export function BioEditor (props) {
 
   return (
     <LexicalComposer initialConfig={config}>
-      <Editor onSubmit={onSubmitBio}>
+      <Editor onSubmit={onSubmitCatch}>
         <article className={config.theme.article}>
           {!readOnly && (
             <Toolbar
-              title={`${!readOnly ? 'Editing ' : ''}${post.title || 'Post'}`}
+              title={
+                title ?? `${!readOnly ? 'Editing ' : ''}${post.title || 'Post'}`
+              }
             />
           )}
 
