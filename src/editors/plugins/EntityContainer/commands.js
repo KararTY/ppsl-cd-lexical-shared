a700b98f9047cd564c5bcf5d4fdf9a074c4e1bb4
Karar Al-Remahy <alremahykarar@ymail.com>
2025-01-04 03:01:50 +0100

    refactor transforms

--- src/editors/plugins/EntityContainer/commands.js
+++ src/editors/plugins/EntityContainer/commands.js
@@ -1,3 +1,180 @@
-import { createCommand } from 'lexical'
+import {
+  $createParagraphNode,
+  $createTextNode,
+  $getRoot,
+  COMMAND_PRIORITY_LOW,
+  createCommand,
+  ElementNode,
+  ParagraphNode,
+  RootNode,
+  TextNode
+} from 'lexical'
+import { mergeRegister } from '@lexical/utils'
+
+import { $createEntityImageNode, EntityImageNode } from '../EntityImage/node.js'
+import {
+  $createEntityShortDescriptionNode,
+  EntityShortDescriptionNode
+} from '../EntityShortDescription/node.js'
+import {
+  $createEntityLongDescriptionNode,
+  EntityLongDescriptionNode
+} from '../EntityLongDescription/node.js'
+import { $createEntityContainerNode, EntityContainerNode } from './node.js'
 
 export const RESET_EDITOR = createCommand('resetEditor')
+
+export const INSERT_ENTITYCONTAINER_COMMAND = createCommand(
+  'insertEntityContainer'
+)
+
+/**
+ * @param {import('lexical').LexicalEditor} editor
+ */
+export const insertEntityContainerCommand = (editor) =>
+  editor.registerCommand(
+    INSERT_ENTITYCONTAINER_COMMAND,
+    () => {
+      editor.update(
+        () => {
+          const entityImage = $createEntityImageNode({ src: '' })
+          const entityShortDescription =
+            $createEntityShortDescriptionNode().append($createParagraphNode())
+
+          const entityLongDescription =
+            $createEntityLongDescriptionNode().append($createParagraphNode())
+
+          const entityContainer = $createEntityContainerNode().append(
+            entityImage,
+            entityShortDescription,
+            entityLongDescription
+          )
+          $getRoot().clear().append(entityContainer)
+
+          entityShortDescription.selectEnd()
+        },
+        { tag: 'historic' }
+      )
+
+      return true
+    },
+    COMMAND_PRIORITY_LOW
+  )
+
+/**
+ * @param {import('lexical').LexicalNode} node
+ */
+const isText = (node) =>
+  node instanceof TextNode || node instanceof ParagraphNode
+
+/**
+ * @param {import('lexical').ElementNode} node
+ */
+function removeDirectDescendantTextNodes (node) {
+  const children = node.getChildren()
+
+  for (let index = 0; index < children.length; index++) {
+    const child = children[index]
+    if (isText(child)) {
+      /**
+       * @type {ElementNode}
+       */
+      const elementSibling = children.find(
+        (sibling) => sibling instanceof ElementNode && !isText(sibling)
+      )
+
+      if (elementSibling) {
+        elementSibling.append(child)
+      } else {
+        child.remove()
+      }
+    }
+  }
+}
+
+/**
+ * @param {import('lexical').LexicalEditor} editor
+ */
+export const nodeTransforms = (editor) =>
+  mergeRegister(
+    editor.registerNodeTransform(EntityContainerNode, (node) => {
+      removeDirectDescendantTextNodes(node)
+
+      const hasLongDescription = node
+        ?.getChildren()
+        .find((child) => child instanceof EntityLongDescriptionNode)
+
+      if (!hasLongDescription) {
+        node.append($createEntityLongDescriptionNode())
+      }
+    }),
+
+    editor.registerNodeTransform(EntityShortDescriptionNode, (node) => {
+      const parentNode = node.getParent()
+      const hasShortDescriptions = parentNode
+        ?.getChildren()
+        .filter((child) => child instanceof EntityShortDescriptionNode)
+
+      if (
+        !parentNode ||
+        !(parentNode instanceof EntityContainerNode) ||
+        hasShortDescriptions.length > 1
+      ) {
+        node.replace($createParagraphNode(), true)
+      }
+    }),
+
+    editor.registerNodeTransform(EntityShortDescriptionNode, (node) => {
+      const hasParagraph = node
+        ?.getChildren()
+        .find((child) => child instanceof ParagraphNode)
+
+      if (!hasParagraph) {
+        node.append($createParagraphNode())
+      }
+    }),
+
+    editor.registerNodeTransform(EntityLongDescriptionNode, (node) => {
+      const parentNode = node.getParent()
+      const hasLongDescriptions = parentNode
+        ?.getChildren()
+        .filter((child) => child instanceof EntityLongDescriptionNode)
+
+      if (
+        !parentNode ||
+        !(parentNode instanceof EntityContainerNode) ||
+        hasLongDescriptions.length > 1
+      ) {
+        node.replace($createParagraphNode(), true)
+      }
+    }),
+
+    editor.registerNodeTransform(EntityLongDescriptionNode, (node) => {
+      const hasParagraph = node
+        ?.getChildren()
+        .find((child) => child instanceof ParagraphNode)
+
+      if (!hasParagraph) {
+        node.append($createParagraphNode())
+      }
+    }),
+
+    editor.registerNodeTransform(EntityImageNode, (node) => {
+      const parentNode = node.getParent()
+      const hasImageNodes = parentNode
+        ?.getChildren()
+        .filter((child) => child instanceof EntityImageNode)
+
+      if (
+        !parentNode ||
+        !(parentNode instanceof EntityContainerNode) ||
+        hasImageNodes.length > 1
+      ) {
+        node.replace($createTextNode())
+      }
+    }),
+
+    editor.registerNodeTransform(RootNode, (node) =>
+      removeDirectDescendantTextNodes(node)
+    )
+  )
--- src/editors/plugins/EntityContainer/utils.js
+++
@@ -1,5 +0,0 @@
-import { createCommand } from 'lexical'
-
-export const INSERT_ENTITYCONTAINER_COMMAND = createCommand(
-  'insertEntityContainer'
-)
